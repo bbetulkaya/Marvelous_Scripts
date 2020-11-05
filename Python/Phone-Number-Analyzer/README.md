31d567254feeab5b6230bddacd83727aea0e5d1d
Betul KAYA <55402564+bbetulkaya@users.noreply.github.com>
2020-11-05 21:18:04 +0300

    add phone number analyzer python script

---
+++ Python/Phone-Number-Analyzer/README.md
@@ -0,0 +1,21 @@
+# Phone Number Analyzer
+
+Phone Number Analyzer is a Python script to show entered phone number Country and Supplier details in the console
+
+## Library used
+- phonenumbers
+- Tabulate
+
+## Setup the environment
+
+Install the packages listed in `requirements.txt` using `pip`
+
+```bash
+pip install -r requirements.txt
+```
+
+## Usage
+
+```bash
+python phone-number-analyzer.py
+```
---
+++ Python/Phone-Number-Analyzer/phone-number-analyzer.py
@@ -0,0 +1,18 @@
+import phonenumbers
+from phonenumbers import carrier
+from phonenumbers import geocoder
+from tabulate import tabulate
+
+
+def number_check(phone_number):
+    number = phonenumbers.parse(phone_number)
+    description = geocoder.description_for_number(number, "en")
+    supplier = carrier.name_for_number(number, "en")
+    info = [["Country", "Supplier"],
+            [description, supplier]]
+    return tabulate(info, headers="firstrow", tablefmt="pretty")
+
+
+if __name__ == "__main__":
+    number = input("Enter number:")
+    print(number_check(number))
